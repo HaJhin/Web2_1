82923d5a772e5fcce6834c36eca25a195464647e
HaJhin <apffksls333@naver.com>
2024-12-04 20:47:32 +0900

    day21

---
+++ src/day36/Example1.java
@@ -0,0 +1,14 @@
+package day36;
+
+public class Example1 {
+    public static void main(String[] args) {
+
+        // [1] 변수
+
+       method(3);
+
+    }
+    public static void method(int i) {
+        System.out.println("입력한 숫자는 "+ i);
+    }
+} // class ed
---
+++ src/day36/Example2.java
@@ -0,0 +1,15 @@
+package day36;
+
+import java.util.ArrayList;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int[] n2 = {10,20,30};
+
+        ArrayList<Integer> 변수3 = new ArrayList<>();
+        변수3.add(10);
+        변수3.add(20);
+        변수3.add(30);
+    }
+}
